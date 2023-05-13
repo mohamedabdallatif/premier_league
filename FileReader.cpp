354e1710de8015a532c62a0d4737c74172072b55
unknown <mshoman01020@gmail.com>
2023-05-13 22:04:38 +0300

    Edit in FileReader

--- FileReader.cpp
+++ FileReader.cpp
@@ -1,67 +1,55 @@
-<!-- - to edit go to this site: https://github.com/Ileriayo/markdown-badges/blob/master/README.md ... -->
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define TIME ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+const ll oo = 1e18, N = 1e9 + 1;
+struct MatchData {
+    int roundNumber;
+    string date;
+    string homeTeam;
+    string awayTeam;
+    int homeGoals;
+    int awayGoals;
+    char result;
+};
 
-<h1 align = "center"> Hi 👋, I'm Mostafa Mahmoud Mohamed </h1>
-<h2 align="center">"&ltJust Engineer/&gt"</h2>
+int main() {
+    // read match data from CSV file
+    ifstream file("matches.csv");
+    string line;
+    getline(file, line);
+    while (getline(file, line)) {
+        stringstream ss(line);
+        string field;
+        vector<string> fields;
+        while (getline(ss, field, ',')) {
+            fields.push_back(field);
+        }
+        if(fields[4] == "*")    continue;
+        cout << stoi(fields[0]) <<"\t\t\t" << fields[1] << "\t\t\t" << fields[2] <<"\t\t\t"<< fields[3] << "\t\t\t" << stoi(fields[4]) <<"\t\t\t"<< stoi(fields[5]) <<  "\t\t\t" << fields[6][0] << '\n';
+    }
+    ifstream file("matches.csv");
+    string line;
+    getline(file, line);
+    while (getline(file, line)) {
+        stringstream ss(line);
+        string field;
+        vector<string> fields;
+        while (getline(ss, field, ',')) {
+            fields.push_back(field);
+        }
+        if(fields[4] == "-")    continue;
+        // cout << stoi(fields[0]) <<"\t\t\t" << fields[1] << "\t\t\t" << fields[2] <<"\t\t\t"<< fields[3] << "\t\t\t" << stoi(fields[4]) <<"\t\t\t"<< stoi(fields[5]) <<  "\t\t\t" << fields[6][0] << '\n';
+        MatchData match;
 
+        match.roundNumber = stoi(fields[0]);
+        match.date = fields[1];
+        match.homeTeam = fields[2];
+        match.awayTeam = fields[3];
+        match.homeGoals = stoi(fields[4]);
+        match.awayGoals = stoi(fields[5]);
+        match.result = fields[6][0];
+        // Extract match date (if needed)
+    }
 
-
-
-Here are some ideas to get you started:
-I’m currently learning Mobile Development
-- 🔭 I’m currently working on [Youth-Care WebSite](https://github.com/mohamedmamdouh22/Youth-care)
-- 🌱 I’m currently learning **JAVA** 
-<!-- - 👯 I’m looking to collaborate on ... -->
-<!-- - 🤔 I’m looking for help with ... -->
-<!-- - 💬 Ask me about ... -->
-- 📫 How to reach me: **mshoman01020@gmail.com**
-<!-- - 😄 Pronouns:  -->
-- ⚡ Fun fact: **You will follow me 😄......**
-
-## Languages:
-![python](https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white)
-![C++](https://img.shields.io/badge/c++-%2300599C.svg?style=for-the-badge&logo=c%2B%2B&logoColor=white)
-
-## Front-End Development:
-![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)
-![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)
-![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
-
-## Back-End Development:
-
-![MySQL](https://img.shields.io/badge/mysql-%2300f.svg?style=for-the-badge&logo=mysql&logoColor=white)
-
-## Cross Platform Development: 
-![Flutter](https://img.shields.io/badge/Flutter-%2302569B.svg?style=for-the-badge&logo=Flutter&logoColor=white)
-
-
-## Design Tools:
-![Adobe XD](https://img.shields.io/badge/Adobe%20XD-470137?style=for-the-badge&logo=Adobe%20XD&logoColor=#FF61F6)
-![Adobe Photoshop](https://img.shields.io/badge/adobe%20photoshop-%2331A8FF.svg?style=for-the-badge&logo=adobe%20photoshop&logoColor=white)
-![Adobe Illustrator](https://img.shields.io/badge/adobe%20illustrator-%23FF9A00.svg?style=for-the-badge&logo=adobe%20illustrator&logoColor=white)
-
-## Top Technologies: 
-![Windows Terminal](https://img.shields.io/badge/Windows%20Terminal-%234D4D4D.svg?style=for-the-badge&logo=windows-terminal&logoColor=white)
-![Git](https://img.shields.io/badge/git-%23F05033.svg?style=for-the-badge&logo=git&logoColor=white)
-
-
-## Workspace Setup:
-![Visual Studio Code](https://img.shields.io/badge/Visual%20Studio%20Code-0078d7.svg?style=for-the-badge&logo=visual-studio-code&logoColor=white)
-
-## Reach me out!
-[![Codeforces](https://img.shields.io/badge/Codeforces-445f9d?style=for-the-badge&logo=Codeforces&logoColor=white)](https://codeforces.com/profile/Mostafa.Shouman)
-[![linked-in](https://img.shields.io/badge/Linked_In-0077B5?style=for-the-badge&logo=LinkedIn&logoColor=white)](https://www.linkedin.com/in/mostafa-mahmoud-shoman/)
-[![github](https://img.shields.io/badge/GitHub-000000?style=for-the-badge&logo=GitHub&logoColor=white)](https://github.com/Shoumanx)
-[![gmail](https://img.shields.io/badge/Gmail-D14836?style=for-the-badge&logo=Gmail&logoColor=white)](mailto:mshoman01020@gmail.com)
-[![Facebook](https://img.shields.io/badge/Facebook-%231877F2.svg?style=for-the-badge&logo=Facebook&logoColor=white)](https://www.facebook.com/profile.php?id=100046760912614)
-[![Telegram](https://img.shields.io/badge/Telegram-2CA5E0?style=for-the-badge&logo=telegram&logoColor=white)](https://t.me/MostafaShoman)
-
-## 📈 Stats:
-
-<div align="center">
-    <img src="https://github-profile-trophy.vercel.app/?username=Shoumanx&row=1&column=6&margin-h=8&theme=darkhub&count_private=true&margin-w=15&no-frame=true" alt="profile trophies" />
-</div>
-<p align="center"> <img src=https://github-readme-stats.vercel.app/api?username=Shoumanx&show_icons=true alt=rahuldkjain /> </p>
-<p align="center"> <img src="https://visitor-badge.laobi.icu/badge?page_id=Shoumanx" alt="visitors"></p>
-
-
-
+}
